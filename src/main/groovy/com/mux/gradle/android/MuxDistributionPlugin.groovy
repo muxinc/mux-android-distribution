c06056a344671bcf9a2d4ec8fe662ca0ff68ebfd
Emily Dixon <edixon@mux.com>
2022-08-19 09:43:43 -0700

    Initial commit: Plugin defs and build script

---
+++ src/main/groovy/com/mux/gradle/android/MuxDistributionPlugin.groovy
@@ -0,0 +1,11 @@
+package com.mux.gradle.android
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MuxDistributionPlugin implements Plugin<Project> {
+  @Override
+  void apply(Project project) {
+
+  }
+}
