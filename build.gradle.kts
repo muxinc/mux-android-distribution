d98b80f28ccac5b11547ecb9675878c1d276b35d
Emily Dixon <edixon@mux.com>
2023-12-06 22:25:21 -0800

    good

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@
 
 tasks.register("assemble") {
   dependsOn(gradle.includedBuild("distribution").task(":distribution:assemble"))
-  dependsOn(gradle.includedBuild("dokka").task(":dokka:assemble"))
   dependsOn(gradle.includedBuild("distribution-old").task(":distribution-old:assemble"))
-  val buildGitUtils = dependsOn(gradle.includedBuild("git-utils").task(":git-utils:assemble"))
   dependsOn(gradle.includedBuild("mux-artifactory").task(":mux-artifactory:assemble"))
+  val buildGitUtils = dependsOn(gradle.includedBuild("git-utils").task(":git-utils:assemble"))
+  buildGitUtils.dependsOn(gradle.includedBuild("dokka").task(":dokka:assemble"))
   buildGitUtils.dependsOn(gradle.includedBuild("android-publication").task(":android-publication:assemble"))
 }
